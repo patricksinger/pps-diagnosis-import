b6795377913551a387b484850bb7c4c1167c20fe
Patrick Singer <patricksinger@gmail.com>
2018-08-08 14:12:42 -0500

    refactored eventhandler functions

--- howto.js
+++ howto.js
@@ -1,45 +1,51 @@
-
-
-var programArray = ["CSP", "CCS", "OP", "CRISIS"];
+// set variables for scope
 var loadedArray = [];
 var currentIndex = "";
 var currentCode = "";
 var currentValue = "";
 
-document.getElementById("addProgram").addEventListener("click", function() {
+document.getElementById("addProgram").addEventListener("click", function () {
+  // TODO: refactor to separate function handler
   currentCode = document.getElementById("programCode").value;
   currentValue = document.getElementById("programValue").value;
 
-  if (currentCode && currentValue) {
-    updateProgramList(currentCode, currentValue);
+  if (currentCode) {
+    updateProgramList(currentCode, currentValue ? currentValue : currentCode);
   } else {
+    // TODO: output to alert style component on page
     console.log("No Program Information Entered");
   }
-
 });
 
 document.getElementById("clearProgram").addEventListener("click", clearProgramEntry);
-
 document.getElementById("generateSQL").addEventListener("click", generateSQL);
 
 function updateProgramList(code, value) {
   // TODO: check if already exists
   if (currentIndex) {
-    // update 
+    // update to existing entry
     loadedArray[currentIndex].code = document.getElementById("programCode").value;
     loadedArray[currentIndex].value = document.getElementById("programValue").value;
     currentIndex = '';
-  } else if (loadedArray.filter(program => program.code === code && program.value === value).length > 0) {
-    console.log("Program Already Exists");
+  } else if (loadedArray.filter(program => program.code === code || program.value === value).length > 0) {
+    // code or value already present in list
+    // TODO: output to alert style component on page
+    console.log("Program Code or Value Already Entered in List");
   } else {
-    loadedArray.push({code: code, value: value});
+    // add to list
+    loadedArray.push({
+      code: code,
+      value: value
+    });
   }
 
+  // clean up and re-render list
   clearProgramEntry();
   renderProgramList()
 }
 
 function clearProgramEntry() {
+  // reset entry fields and buttons to original state
   document.getElementById("programCode").value = '';
   document.getElementById("programValue").value = '';
   document.getElementById("addProgram").innerHTML = "Add Program"
@@ -51,65 +57,64 @@ function renderProgramList() {
 
   document.getElementById("programList").innerHTML = '';
 
-  loadedArray.forEach(function(program, index) {
+  loadedArray.forEach(function (program, index) {
 
     listElement = document.createElement("li");
-    listElement.setAttribute("draggable","true");
-    listElement.setAttribute("class","sortable-bulk");
-    listElement.setAttribute("id",index);
-    listElement.setAttribute("pcode",program.code);
-    listElement.setAttribute("pvalue",program.value);
+    listElement.setAttribute("draggable", "true");
+    listElement.setAttribute("class", "sortable-bulk");
+    listElement.setAttribute("id", index);
+    listElement.setAttribute("pcode", program.code);
+    listElement.setAttribute("pvalue", program.value);
 
-    listElementContent = document.createTextNode("Priority : " + (index+1) + " - "  + program.code + " / " + program.value);
+    listElementContent = document.createTextNode("Priority : " + (index + 1) + " - " + program.code + " / " + program.value);
     listElement.appendChild(listElementContent);
     document.getElementById("programList").appendChild(listElement);
 
   });
 }
 
-
 var dragging = null;
 
-document.getElementById("programList").addEventListener('dragstart', function(event) {
-		dragging = event.target;
-    //event.dataTransfer.setData('text/html', dragging);
+document.getElementById("programList").addEventListener('dragstart', function (event) {
+  dragging = event.target;
+  //event.dataTransfer.setData('text/html', dragging);
 });
 
-document.getElementById("programList").addEventListener('dragover', function(event) {
-    event.preventDefault();
-    //window.requestAnimationFrame(function(){
-    	var bounding = event.target.getBoundingClientRect()
-      var offset = bounding.y + (bounding.height/2);
-      if ( event.clientY - offset > 0 ) {
-      	event.target.style['border-bottom'] = 'solid 4px blue';
-        event.target.style['border-top'] = '';
-      } else {
-        event.target.style['border-top'] = 'solid 4px blue';
-        event.target.style['border-bottom'] = '';
-      }
-    //});
-});
-
-document.getElementById("programList").addEventListener('dragleave', function(event) {
-    event.target.style['border-bottom'] = '';
+document.getElementById("programList").addEventListener('dragover', function (event) {
+  event.preventDefault();
+  //window.requestAnimationFrame(function(){
+  var bounding = event.target.getBoundingClientRect()
+  var offset = bounding.y + (bounding.height / 2);
+  if (event.clientY - offset > 0) {
+    event.target.style['border-bottom'] = 'solid 4px blue';
     event.target.style['border-top'] = '';
+  } else {
+    event.target.style['border-top'] = 'solid 4px blue';
+    event.target.style['border-bottom'] = '';
+  }
+  //});
 });
 
-document.getElementById("programList").addEventListener('drop', function(event) {
-    event.preventDefault();
-    if ( event.target.style['border-bottom'] !== '' ) {
-      event.target.style['border-bottom'] = '';
-      event.target.parentNode.insertBefore(dragging, event.target.nextSibling);
-    } else {
-      event.target.style['border-top'] = '';
-      event.target.parentNode.insertBefore(dragging, event.target);
-    }
-
-    // reindex list based on new order of items
-    reindexList();
+document.getElementById("programList").addEventListener('dragleave', function (event) {
+  event.target.style['border-bottom'] = '';
+  event.target.style['border-top'] = '';
 });
 
-document.getElementById("programList").addEventListener("dblclick", function(event) {
+document.getElementById("programList").addEventListener('drop', function (event) {
+  event.preventDefault();
+  if (event.target.style['border-bottom'] !== '') {
+    event.target.style['border-bottom'] = '';
+    event.target.parentNode.insertBefore(dragging, event.target.nextSibling);
+  } else {
+    event.target.style['border-top'] = '';
+    event.target.parentNode.insertBefore(dragging, event.target);
+  }
+
+  // reindex list based on new order of items
+  reindexList();
+});
+
+document.getElementById("programList").addEventListener("dblclick", function (event) {
 
   // TODO: load target item in to editor view
   currentIndex = event.target.getAttribute("id");
@@ -122,26 +127,32 @@ document.getElementById("programList").addEventListener("dblclick", function(eve
 
 
 function reindexList() {
-    var list = document.getElementById("programList");
-    var listItems = list.getElementsByTagName("li");
+  var list = document.getElementById("programList");
+  var listItems = list.getElementsByTagName("li");
 
-    loadedArray = [];
+  loadedArray = [];
 
-    for (var i=0; i < listItems.length; i++) {
-      listItems[i].setAttribute("id", i);
-      loadedArray.push({code: listItems[i].getAttribute("pcode"), value: listItems[i].getAttribute("pvalue")});
-    };
+  for (var i = 0; i < listItems.length; i++) {
+    listItems[i].setAttribute("id", i);
+    loadedArray.push({
+      code: listItems[i].getAttribute("pcode"),
+      value: listItems[i].getAttribute("pvalue")
+    });
+  };
 
-    // reload list
-    renderProgramList();
+  // reload list
+  renderProgramList();
 }
-
 
 
 function generateSQL() {
 
-  var programOutput = loadedArray.reverse().map(function(program, index) {
-    return {code: program.code, value: program.value, weight: (index * 100)+100}
+  var programOutput = loadedArray.reverse().map(function (program, index) {
+    return {
+      code: program.code,
+      value: program.value,
+      weight: (index * 100) + 100
+    }
   });
 
   var caseStatementPrograms = "";
@@ -170,7 +181,7 @@ from (
 		select
 		case 
       ${caseStatementPrograms}
-			else -1
+			else 0
 		end as program_ranking,
 		case 
 			when e.date_of_discharge IS NULL then 20
@@ -191,10 +202,6 @@ where DIAGC.code_set_code = 'ICD10' and DIAGE.diagnosis_status_code = '1' and (D
 group by E.PATID, E.EPISODE_NUMBER, DIAGR.FACILITY, DIAGE.DiagnosisRecord, DIAGR.date_of_diagnosis, DIAGE.billing_order, DIAGC.diagnosis_code, DIAGC.diagnosis_value
 ) as diagnosis on pps.PATID = diagnosis.PATID`
 
-
-  
-
   document.getElementById("sqlOutput").value = sql;
 
 }
-
